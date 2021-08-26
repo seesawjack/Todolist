27f3570ab779582be160413f692b466bf08fe077
seesawjack <82428741+seesawjack@users.noreply.github.com>
2021-08-26 13:22:48 +0800

    08/26 13:22

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-
-//給每個 list 加上取消的 icon
+//給每個 list 加上編輯跟取消的 icon
 var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var icon = `<i class="fas fa-trash"></i>`
-  span.className = "close";
-  span.innerHTML= icon;
-  myNodelist[i].appendChild(span);
+for (i = 0; i < myNodelist.length; i++) {       //加上編輯的 icon
+  const btn = document.createElement("button");
+  const icon = `<i class="fas fa-edit"></i>`
+  btn.className = "edit";
+  btn.innerHTML= icon;
+  myNodelist[i].appendChild(btn);
+}
+for (i = 0; i < myNodelist.length; i++) {       //加上刪除的 icon
+  const btn = document.createElement("button");
+  const icon = `<i class="fas fa-trash"></i>`
+  btn.className = "close";
+  btn.innerHTML= icon;
+  myNodelist[i].appendChild(btn);
 }
 
-// 按下 icon 後會移除該 list
+var edit = document.getElementsByClassName("edit");   //可編輯動作
+var i;
+for (i = 0; i < edit.length; i++) {
+  edit[i].onclick = function() {
+    this.previousElementSibling.contentEditable = true;
+    this.previousElementSibling.focus();
+  }
+}
+
+
+// 按下取消的 icon 後會移除該 list
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -21,9 +37,9 @@ for (i = 0; i < close.length; i++) {
 
 //點擊 list 時加上 classname checked
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+list.addEventListener('click', function(e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
   }
 }, false);
 
@@ -37,10 +53,11 @@ inputTxt.addEventListener('keypress',function(e){
 
 //按下新增鍵後增加一個 list
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myinput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myinput").value;
+  const span = document.createElement("span");
+  span.textContent = inputValue;
+  li.appendChild(span);
   
   if (inputValue === '') {
     alert("請輸入內容");
@@ -48,25 +65,31 @@ function newElement() {
     document.getElementById("myul").appendChild(li);
   }
 
-  document.getElementById("myinput").value = "";
+  document.getElementById("myinput").value = "";  //清空 input 的值
+  
+  const editBtn = document.createElement("button");  //新增 edit 的按鈕
+  editBtn.innerHTML = `<i class="fas fa-edit"></i>`
+  li.appendChild(editBtn);
 
-  var span = document.createElement("SPAN");
-  var icon = `<i class="fas fa-trash"></i>`
-  span.className = "close";
-  span.innerHTML = icon;
-  li.appendChild(span);
+  const delBtn = document.createElement("button");  //新增 delete 的按鈕
+  delBtn.innerHTML = `<i class="fas fa-trash"></i>`
+  li.appendChild(delBtn);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      this.parentElement.remove();
-    }
-  }
+  editBtn.addEventListener("click", function () {  //按下 edit 編輯該 list
+      this.previousElementSibling.contentEditable = true;
+      this.previousElementSibling.focus();
+  });
+  delBtn.addEventListener("click", function () {  //按下 delete 刪除該 list
+    this.parentElement.remove();
+  });
 }
 
 //按刪除鍵刪除全部代辦事項
 let clearBtn = document.querySelector('.clearBtn');
 clearBtn.addEventListener('click',function(){
-    var list = document.querySelector('ul');
-    list.innerHTML="";
-    
+    const check = window.confirm('您確定要刪除全部嗎');
+    if (check == true) {
+      const list = document.querySelector('ul');
+      list.innerHTML="";
+    }
 })
